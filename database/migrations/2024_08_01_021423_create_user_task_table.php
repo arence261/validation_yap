f729fe0886a125f5932a1df871d54de95c810780
arence261 <rencetohoy1@gmail.com>
2024-08-01 11:35:26 +0800

    panagition

--- database/migrations/2024_08_01_021423_create_user_task_table.php
+++ database/migrations/2024_08_01_021423_create_user_task_table.php
@@ -5,23 +5,30 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
-{
+{     
     /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('user_task', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+       Schema::create('user_task', function (Blueprint $table) {
+           $table->id();
+           $table->integer('user_id')->nullable();
+           $table->string('task_name')->nullable();
+           $table->enum('status', ['Pending, On process','Complete','Cancelled'])->nullable();
+           $table->text('description')->nullable();
+           $table->date('deadline')->nullable();
+           $table->date('complete_at')->nullable();
+           $table->timestamps();
+       });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('user_task');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+       Schema::dropIfExists('user_task');
+   }
+
 };
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserTaskController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('user_task', UserTaskController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
